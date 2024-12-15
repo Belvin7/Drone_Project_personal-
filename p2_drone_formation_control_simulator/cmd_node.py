2dcdca4e80aebb6c3bffa4c3f0783caab5e40b2d
Christian Grüneberg <christian.grueneberg@ovgu.de>
2024-12-15 01:04:41 +0100

    added function for simple_gui

--- p2_drone_formation_control_simulator/cmd_node.py
+++ p2_drone_formation_control_simulator/cmd_node.py
@@ -1,87 +1,68 @@
 import rclpy
-import math 
 import time 
-import errno 
 from rclpy.node import Node
 
 from std_msgs.msg import String
 from geometry_msgs.msg import TwistStamped
-from builtin_interfaces.msg import Time 
-from ardupilot_msgs.msg import GlobalPosition 
-from geographic_msgs.msg import GeoPoseStamped 
-from geopy import distance 
-from geopy import point 
-from ardupilot_msgs.srv import ArmMotors 
-from ardupilot_msgs.srv import ModeSwitch 
-from ardupilot_msgs.srv import Takeoff 
  
-COPTER_MODE_GUIDED = 4 
-TAKEOFF_ALT = 20.0 
-
 class CmdVel(Node):
-
     def __init__(self):
         super().__init__('cmd_vel')
 
         # variables
-        self.i = 0
+        #self.i = 0
         self.send_vel = False
+        self.msg = TwistStamped()
+        self.msg.header.stamp = self.get_clock().now().to_msg()
+        self.msg.header.frame_id = 'base_link'
+        self.msg.twist.linear.x = 0.
+        self.msg.twist.linear.y = 0.
+        self.msg.twist.linear.z = 0.
+        self.msg.twist.angular.x = 0.
+        self.msg.twist.angular.y = 0.
+        self.msg.twist.angular.z = 0.
 
         # publisher
         self.cmd_vel_publisher = self.create_publisher(TwistStamped, 'ap/cmd_vel', 10)
-
-        # client
-
 
         # subscriber
         self.cmd_subscriber = self.create_subscription(String, 'p2/cmd', self.cmd_listener,10)
 
         # timer with callback
-        timer_period = 0.5  # seconds
+        timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-    def cmd_listener(self, msg):
-        self.get_logger().info('cmd = %s' % msg.data)
-        if msg.data == 'takeoff':
-            try:
-                if not self.switch_mode_with_timeout(COPTER_MODE_GUIDED, rclpy.duration.Duration(seconds=20)):
-                    raise RuntimeError("Unable to switch to guided mode")
-                # Block till armed, which will wait for EKF3 to initialize
-                if not self.arm_with_timeout(rclpy.duration.Duration(seconds=30)):
-                    raise RuntimeError("Unable to arm")
-
-                # Block till in takeoff
-                if not self.takeoff_with_timeout(TAKEOFF_ALT, rclpy.duration.Duration(seconds=20)):
-                    raise RuntimeError("Unable to takeoff")
-
-                is_ascending_to_takeoff_alt = True
-                while is_ascending_to_takeoff_alt:
-                    rclpy.spin_once(self)
-                    time.sleep(1.0)
-
-                    is_ascending_to_takeoff_alt = self.get_cur_geopose().pose.position.altitude < TAKEOFF_ALT
-
-                if is_ascending_to_takeoff_alt:
-                    raise RuntimeError("Failed to reach takeoff altitude")
-
-            except KeyboardInterrupt:
-                pass
+    def cmd_listener(self, msg_sub):
+        self.get_logger().info('cmd = %s' % msg_sub.data)
+        if msg_sub.data == 'start':
+            self.msg.twist.linear.x = 3.0
+            self.msg.twist.angular.z = 0.0
             self.send_vel = True
-
+        elif msg_sub.data == 'right':
+            self.msg.twist.linear.x = 0.0
+            self.msg.twist.angular.z = -1.0
+            self.send_vel = True
+        elif msg_sub.data == 'left':
+            self.msg.twist.linear.x = 0.0
+            self.msg.twist.angular.z = 1.0
+            self.send_vel = True
+        elif msg_sub.data == 'stop':
+            self.send_vel = False 
+ 
     def timer_callback(self):
         if self.send_vel:
-            msg = TwistStamped()
-            msg.header.stamp = self.get_clock().now().to_msg()
-            msg.header.frame_id = 'base_link'
-            msg.twist.linear.x = 3.
-            msg.twist.linear.y = 0.
-            msg.twist.linear.z = 0.
-            msg.twist.angular.x = 0.
-            msg.twist.angular.y = 0.
-            msg.twist.angular.z = 0.
-            self.cmd_vel_publisher.publish(msg)
+            #msg = TwistStamped()
+            self.msg.header.stamp = self.get_clock().now().to_msg()
+            #msg.header.frame_id = 'base_link'
+            #msg.twist.linear.x = 3.
+            #msg.twist.linear.y = 0.
+            #msg.twist.linear.z = 0.
+            #msg.twist.angular.x = 0.
+            #msg.twist.angular.y = 0.
+            #msg.twist.angular.z = 0.
+            self.cmd_vel_publisher.publish(self.msg)
             #self.get_logger().info('Publishing: "%s"' % msg)
-            self.i += 1
+            #self.i += 1
 
 
 def main(args=None):
--- p2_drone_formation_control_simulator/gui_node.py
+++ p2_drone_formation_control_simulator/gui_node.py
@@ -1,54 +1,229 @@
 import rclpy
+import time
 import tkinter as tk
 from rclpy.node import Node
+
 from std_msgs.msg import String
 from geometry_msgs.msg import TwistStamped
+from ardupilot_msgs.srv import ArmMotors
+from ardupilot_msgs.srv import ModeSwitch
+from ardupilot_msgs.srv import Takeoff
 
+#COPTER_MODE_GUIDED = 4 
+#TAKEOFF_ALT = 20.0 
 
 class P2(Node):
-
     def __init__(self):
         super().__init__('p2')
+
+        # publisher should be used to set the velocities
         self.cmd_publisher = self.create_publisher(String, 'p2/cmd', 10)
+
+        # clients
+        self.declare_parameter("mode_topic", "/ap/mode_switch")
+        self._mode_topic = self.get_parameter("mode_topic").get_parameter_value().string_value
+        self._client_mode_switch = self.create_client(ModeSwitch, self._mode_topic)
+        while not self._client_mode_switch.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('mode switch service not available, waiting again...')
+
+        self.declare_parameter("arm_topic", "/ap/arm_motors")
+        self._arm_topic = self.get_parameter("arm_topic").get_parameter_value().string_value
+        self._client_arm = self.create_client(ArmMotors, self._arm_topic)
+        while not self._client_arm.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('arm service not available, waiting again...')
+
+        self.declare_parameter("takeoff_service", "/ap/experimental/takeoff")
+        self._takeoff_topic = self.get_parameter("takeoff_service").get_parameter_value().string_value
+        self._client_takeoff = self.create_client(Takeoff, self._takeoff_topic)
+        while not self._client_takeoff.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('takeoff service not available, waiting again...')
 
     def gui(self):
         self.window = tk.Tk()
         self.window.title('P2 Simulator')
 
-        # create cmd button
-        launch_button = tk.Button(self.window, 
-                  text='launch', 
-                  command=self.launch_clicked,
-                  padx=10,
-                  pady=5,
-                  width=15
-        )
-        launch_button.pack(padx=10, pady=0)
 
-        # return-to-home button
+        # guided button
+        guided_button = tk.Button(self.window,
+          text='Guided-mode', 
+          command=self.guided_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        guided_button.pack(padx=10, pady=0)
+
+        # arm button
+        arm_button = tk.Button(self.window,
+          text='Arm', 
+          command=self.arm_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        arm_button.pack(padx=10, pady=0)
+
+        # create takeoff button
+        takeoff_button = tk.Button(self.window, 
+          text='Takeoff', 
+          command=self.takeoff_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        takeoff_button.pack(padx=10, pady=0)
+
+        # start moving button
+        start_button = tk.Button(self.window, 
+          text='Start', 
+          command=self.start_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        start_button.pack(padx=10, pady=0)
+
+        # left turning button
+        left_button = tk.Button(self.window, 
+          text='Left-turning', 
+          command=self.left_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        left_button.pack(padx=10, pady=0)
+
+        # right turning button
+        right_button = tk.Button(self.window, 
+          text='Right-turning', 
+          command=self.right_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        right_button.pack(padx=10, pady=0)
+
+        # stop moving button
+        stop_button = tk.Button(self.window, 
+          text='Stop', 
+          command=self.stop_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        stop_button.pack(padx=10, pady=0)
+
+        # return-to-launch button
         rtl_button = tk.Button(self.window,
-                  text='RTL', 
-                  command=self.rtl_clicked,
-                  padx=10,
-                  pady=5,
-                  width=15
+          text='RTL', 
+          command=self.rtl_clicked,
+          padx=10,
+          pady=5,
+          width=15
         )
         rtl_button.pack(padx=10, pady=0)
 
+        # land button
+        land_button = tk.Button(self.window,
+          text='Land', 
+          command=self.land_clicked,
+          padx=10,
+          pady=5,
+          width=15
+        )
+        land_button.pack(padx=10, pady=0)
+
         self.window.mainloop()
-        
-    def launch_clicked(self):
-       msg = String()
-       msg.data = 'launch'
-       self.cmd_publisher.publish(msg)
-       rclpy.spin_once(self, timeout_sec=0)
+
+    def switch_mode(self, mode):
+        req = ModeSwitch.Request()
+        assert mode in [4, 6, 9]
+        req.mode = mode
+        future = self._client_mode_switch.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
+
+    def switch_mode_with_timeout(self, desired_mode: int, timeout: rclpy.duration.Duration):
+        is_in_desired_mode = False
+        start = self.get_clock().now()
+        while not is_in_desired_mode:
+            result = self.switch_mode(desired_mode)
+            # Handle successful switch or the case that the vehicle is already in expected mode
+            is_in_desired_mode = result.status or result.curr_mode == desired_mode
+            time.sleep(1)
+
+        return is_in_desired_mode
+
+    def arm(self):
+        req = ArmMotors.Request()
+        req.arm = True
+        future = self._client_arm.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
+
+    def arm_with_timeout(self, timeout: rclpy.duration.Duration):
+        armed = False
+        start = self.get_clock().now()
+        while not armed and self.get_clock().now() - start < timeout:
+            armed = self.arm().result
+            time.sleep(1)
+        return armed
+
+    def takeoff(self, alt):
+        req = Takeoff.Request()
+        req.alt = alt
+        future = self._client_takeoff.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
+
+    def takeoff_with_timeout(self, alt: int, timeout: rclpy.duration.Duration):
+        takeoff_success = False
+        start = self.get_clock().now()
+        while not takeoff_success:
+            result = self.takeoff(alt)
+            takeoff_success = result.status
+            time.sleep(1)
+
+        return takeoff_success
+
+    def guided_clicked(self):
+        self.switch_mode_with_timeout(4, rclpy.duration.Duration(seconds=1))
+
+    def arm_clicked(self):
+        self.arm_with_timeout(rclpy.duration.Duration(seconds=1))
+
+    def takeoff_clicked(self):
+        self.takeoff_with_timeout(20.0, rclpy.duration.Duration(seconds=20))
+
+    def start_clicked(self):
+        msg = String()
+        msg.data = 'start'
+        self.cmd_publisher.publish(msg)
+        rclpy.spin_once(self, timeout_sec=0)
+
+    def left_clicked(self):
+        msg = String()
+        msg.data = 'left'
+        self.cmd_publisher.publish(msg)
+        rclpy.spin_once(self, timeout_sec=0)
+
+    def right_clicked(self):
+        msg = String()
+        msg.data = 'right'
+        self.cmd_publisher.publish(msg)
+        rclpy.spin_once(self, timeout_sec=0)
+
+    def stop_clicked(self):
+        msg = String()
+        msg.data = 'stop'
+        self.cmd_publisher.publish(msg)
+        rclpy.spin_once(self, timeout_sec=0)
 
     def rtl_clicked(self):
-       msg = String()
-       msg.data = 'rtl'
-       self.cmd_publisher.publish(msg)
-       rclpy.spin_once(self, timeout_sec=0)
+        self.switch_mode_with_timeout(6, rclpy.duration.Duration(seconds=1))
 
+    def land_clicked(self):
+        self.switch_mode_with_timeout(9, rclpy.duration.Duration(seconds=1))
 
 def main(args=None):
     rclpy.init(args=args)
